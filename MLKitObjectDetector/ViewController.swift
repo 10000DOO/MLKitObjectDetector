76fb4f94069ddebb2ef129270b36c0c701b109c6
10000DOO <gunjoun99@gmail.com>
2024-02-26 10:05:10 +0900

    Initial Commit

---
+++ MLKitObjectDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLKitObjectDetector
+//
+//  Created by 이건준 on 2/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
